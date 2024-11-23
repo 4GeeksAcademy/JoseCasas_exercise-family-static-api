38467ca5d2447aa26951a04a8ed2d6ab7d0ec830
JC-kip <jjcasasm16@gmail.com>
2024-11-23 02:02:14 +0000

    finished

--- src/datastructures.py
+++ src/datastructures.py
@@ -20,16 +20,24 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+       
+        if 'id' not in member:
+             member["id"] = self._generateId()
+
+        self._members.append(member)
+        return member
+        
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        self._members = list(filter(lambda member: member["id"] != id, self._members))
+
 
     def get_member(self, id):
+        for member in self._members:
+            if member["id"] == id:
+                return member
         # fill this method and update the return
-        pass
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
